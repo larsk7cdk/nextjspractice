37044c76a0bed076fd57ec8e6db870b68ac94333
JB6504 <lal@jyskebank.dk>
2024-07-15 11:19:10 +0200

    added loading page

---
+++ src/app/blog/[postid]/loading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function loading() {
+  return (
+    <div className="h-screen bg-yellow-400">
+      <h1 className="text-4xl text-blue-100 text-center">loading...</h1>
+    </div>
+  );
+}
