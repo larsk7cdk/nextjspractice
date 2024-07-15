8a9f81dbf82113ef10faaf740654d4107ea8072d
JB6504 <lal@jyskebank.dk>
2024-07-15 11:04:08 +0200

    added state to auth layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,37 +1,51 @@
-'use client' // client side rendering
+"use client"; // client side rendering
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 const navLinks = [
-    { name: "Register", href: "/register" },
-    { name: "Login", href: "/login" },
-    { name: "Forgot Password", href: "/forgotpassword" }
-]
+  { name: "Register", href: "/register" },
+  { name: "Login", href: "/login" },
+  { name: "Forgot Password", href: "/forgotpassword" },
+];
 
 export default function RootLayout({
-    children
+  children,
 }: Readonly<{
-    children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
 
-    const pathname = usePathname()
+  const [input, setInput] = useState("");
 
-    return (
-        <div>
-            {children}
-            {
-                navLinks.map((link) => {
+  return (
+    <div>
+      <div className="p-4">
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          className="outline rounded-sm"
+        />
+      </div>
 
-                    const isActive = pathname.startsWith(link.href)
+      {children}
+      {navLinks.map((link) => {
+        const isActive = pathname.startsWith(link.href);
 
-                    return (
-                        <Link href={link.href}
-                            key="link.name"
-                            className={isActive ? "text-yellow-400 font-serif font-bold" : "text-blue-400"}>
-                            {link.name}
-                        </Link>
-                    )
-                })}
-        </div>
-    );
+        return (
+          <Link
+            href={link.href}
+            key="link.name"
+            className={
+              isActive
+                ? "text-yellow-400 font-serif font-bold"
+                : "text-blue-400"
+            }
+          >
+            {link.name}
+          </Link>
+        );
+      })}
+    </div>
+  );
 }
